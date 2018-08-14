cb4bc1a29d680260c388ce36db4341c26085338f
Conor <conorward509@hotmail.com>
2018-08-14 19:52:56 +0100

    practice

---
+++ src/arrayofwords/ArrayOfWords.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayofwords;
+
+
+
+/**
+ *
+ * @author conor
+ */
+public class ArrayOfWords {
+   
+  public static void main(String args[]){
+        
+        int a = 1;
+        int b = 2;
+       
+     addNumber ad = new addNumber();
+     int number = ad.add(a, b);
+     System.out.println(number);
+        
+    }
+    
+}
---
+++ src/arrayofwords/addNumber.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrayofwords;
+
+/**
+ *
+ * @author conor
+ */
+public class addNumber {
+    
+    
+     int add(int a ,int b){
+      int ad = a+b;
+        return ad;
+      }
+    
+}
